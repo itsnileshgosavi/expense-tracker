f93318d7680edf0f6ffa28aea728f95aae14e6de
Nilesh Gosavi <nbgosavi5@gmail.com>
2025-03-06 20:39:21 +0530

    fixed bug

--- app/expenses/editExpenseform.tsx
+++ app/expenses/editExpenseform.tsx
@@ -1,6 +1,8 @@
-"use client";  
+"use client";
 
 import React from "react";
+import { Calendar } from "@/components/ui/calendar";
+import { cn } from "@/lib/utils";
 import {
   Form,
   FormControl,
@@ -13,26 +15,40 @@ import {
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useForm } from "react-hook-form";  
-import * as z from "zod";  
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 import { Textarea } from "@/components/ui/textarea";
 import { useExpenses } from "@/hooks/useExpenses";
 import { toast } from "@/hooks/use-toast";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { format } from "date-fns"
+import { CalendarIcon } from "lucide-react"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const formSchema = z.object({
   description: z.string().min(2, {
     message: "Description must be at least 2 characters.",
   }),
-  amount: z.string(), 
-  category: z.enum(["FOOD", "ENTERTAINMENT", 'UTILITIES', 'TRANSPORT',  "OTHER"]).default("OTHER"),
-  date: z.string(),  
+  amount: z.string(),
+  category: z
+    .enum(["FOOD", "ENTERTAINMENT", "UTILITIES", "TRANSPORT", "OTHER"])
+    .default("OTHER"),
+  date: z.string(),
 });
-
 
 type FormValues = z.infer<typeof formSchema>;
 
-const EditExpense = ({fdata}:{fdata:any}) => {
+const EditExpense = ({ fdata }: { fdata: any }) => {
   const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -46,25 +62,21 @@ const EditExpense = ({fdata}:{fdata:any}) => {
   const { updateExpense } = useExpenses();
 
   const onSubmit = async (data: FormValues) => {
-   try {
-    const res =await updateExpense(fdata.id, {
-      ...data,
-      amount: parseFloat(data.amount),
-      date: new Date(data.date),
-    });
-    if(res.id){
-      toast({
-        title: "Success",
-      })
-      form.reset();
+    try {
+      const res = await updateExpense(fdata.id, {
+        ...data,
+        amount: parseFloat(data.amount),
+        date: new Date(data.date),
+      });
+      if (res.id) {
+        toast({
+          title: "Success",
+        });
+        form.reset();
+      }
+    } catch (error) {
+      console.log(error);
     }
-    
-   } catch (error) {
-
-    console.log(error)
-    
-   }
-    
   };
 
   return (
@@ -101,9 +113,8 @@ const EditExpense = ({fdata}:{fdata:any}) => {
           name="category"
           render={({ field }) => (
             <FormItem>
-              
               <FormControl>
-                <Select onValueChange={field.onChange}>
+                <Select value={fdata.category} onValueChange={field.onChange}>
                   <SelectTrigger className="w-[200px] bg-input rounded-lg">
                     <SelectValue placeholder="Select Category" />
                   </SelectTrigger>
@@ -125,9 +136,29 @@ const EditExpense = ({fdata}:{fdata:any}) => {
           name="date"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Date</FormLabel>
               <FormControl>
-                <Input type="date" placeholder="Enter Date of Expense" {...field} />
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[240px] justify-start text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      <CalendarIcon />
+                      {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={new Date(field.value)}
+                      onSelect={() => field.onChange(new Date().toDateString())}
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
               </FormControl>
               <FormMessage />
             </FormItem>
